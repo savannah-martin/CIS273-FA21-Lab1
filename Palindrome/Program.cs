d16b8167f4f07989ebbb0d018eab344f53ac688b
savannah-martin <78399457+savannah-martin@users.noreply.github.com>
2021-10-22 23:17:45 -0500

    palindrome

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -8,12 +8,42 @@ namespace Palindrome
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            LinkedList<int> doublyLinkedList = new LinkedList<int>();
+            for (int i = -15; i < 16; i++)
+            {
+                doublyLinkedList.AddLast(Math.Abs(i));
+            }
+            IsPalindrome(doublyLinkedList);
+
         }
 
         public static bool IsPalindrome<T>(LinkedList<T> linkedList)
         {
-            return false;
-        }
 
+            var rList = new LinkedList<T>();
+
+            var currentNode = linkedList.First;
+
+            if (linkedList.Count == 1)
+            {
+                return true;
+            }
+
+            var pointer1 = rList.First;
+            var pointer2 = linkedList.First;
+
+            while (pointer1 == pointer2)
+            {
+                if(pointer1 != pointer2)
+                {
+                    return false;
+                }
+                
+                continue;
+            }
+            return true;
+
+        }
     }
 }
